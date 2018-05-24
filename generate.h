f3d18bad20ac9bd4a37b0ea29fd9fa4c228d4db0
Kabombom <mantonio@student.dei.uc.pt>
2018-05-25 00:15:58 +0100

    ups

--- generate.h
+++ generate.h
@@ -7,4 +7,4 @@ void generate_code_declaration(node_t *ast);
 void generate_code_func_declaration(node_t *ast);
 void generate_code_func_definition(node_t *ast);
 
-char *get_type(char *type_name);
+char *get_llvm_type(char *type_name);
